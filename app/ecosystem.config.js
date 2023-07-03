83b2f6093c88f7be39a9b549762bd795cfa7f265
Apavayan Sinha <aapav01@gmail.com>
2023-07-03 03:11:26 +0000

    update port for pm2

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'prameyshala',
     exec_mode: 'cluster',
     script: 'node_modules/next/dist/bin/next',
-    args: 'start',
+    args: 'start -p 3001',
     watch: true,
   }]
 }
