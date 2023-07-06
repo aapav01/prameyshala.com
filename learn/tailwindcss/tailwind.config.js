bd83bfef3263e27a1132cdcb09924ca2c93af39f
Apavayan Sinha <info@apavayan.com>
2023-07-07 05:03:29 +0530

    added tailwindcss

---
+++ learn/tailwindcss/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "../templates/**/*.{html,js}",
+    "../**/templates/**/*.{html,js}",
+    "../static/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
