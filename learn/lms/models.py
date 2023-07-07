0e90c1b1f3c5c3f0297be7cf777a466d9fe02867
Apavayan Sinha <info@apavayan.com>
2023-07-07 09:42:52 +0530

    Fixed ForeignKey in models

--- learn/lms/models.py
+++ learn/lms/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 
-class Chapter(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(blank=True, null=True)
-    collectionid = models.CharField(db_column='collectionId', max_length=255, blank=True, null=True)  # Field name made lowercase.
-    course_id = models.CharField(max_length=26)
-    user_id = models.CharField(max_length=26)
-    created_at = models.DateTimeField(blank=True, null=True)
-    updated_at = models.DateTimeField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = 'chapters'
-
-
 class Course(models.Model):
     name = models.CharField(max_length=255)
     short = models.CharField(max_length=255, blank=True, null=True)
@@ -27,7 +11,7 @@ class Course(models.Model):
     before_price = models.FloatField(blank=True, null=True)
     public = models.IntegerField()
     publish_at = models.DateTimeField()
-    user_id = models.CharField(max_length=26)
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
 
@@ -38,13 +22,31 @@ class Course(models.Model):
         db_table = 'courses'
 
 
+class Chapter(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    # Field name made lowercase.
+    collectionid = models.CharField(
+        db_column='collectionId', max_length=255, blank=True, null=True)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(blank=True, null=True)
+    updated_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'chapters'
+
+
 class Enrollment(models.Model):
     bought_price = models.FloatField()
     payment_method = models.CharField(max_length=255, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=7)
-    user_id = models.CharField(max_length=26)
-    course_id = models.CharField(max_length=26)
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
 
@@ -64,8 +66,8 @@ class Lesson(models.Model):
     status = models.CharField(max_length=10)
     platform = models.CharField(max_length=11, blank=True, null=True)
     platform_video_id = models.CharField(max_length=255, blank=True, null=True)
-    user_id = models.CharField(max_length=26)
-    chapter_id = models.CharField(max_length=26, blank=True, null=True)
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    chapter_id = models.ForeignKey(Chapter, on_delete=models.CASCADE)
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
 
@@ -75,6 +77,7 @@ class Lesson(models.Model):
     class Meta:
         db_table = 'lessons'
 
+
 class Payments(models.Model):
     payment_gateway_id = models.CharField(max_length=255)
     gateway = models.CharField(max_length=255)
@@ -83,10 +86,9 @@ class Payments(models.Model):
     user_email = models.CharField(max_length=255, blank=True, null=True)
     amount = models.CharField(max_length=255)
     json_response = models.TextField()
-    enrollment_id = models.CharField(max_length=26, blank=True, null=True)
+    enrollment_id = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
         db_table = 'payments'
-
