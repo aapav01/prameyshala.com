3480e435dc4d56ddfb73ed64fc6242b596cec558
Tejaswa <tejaswadayalsri@gmail.com>
2023-07-08 11:37:06 +0530

    Tried creating views and template

--- learn/app/courses/urls.py
+++ learn/app/courses/urls.py
@@ -1,3 +1,6 @@
 from django.urls import include, path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('classes/',views.classes,name='classes')
+]
--- learn/app/courses/views.py
+++ learn/app/courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.views import View
+from .models  import Classes
 # Create your views here.
+def classes(request):
+    classes = Classes.objects.all()
+    context = {'classes':classes}
+    return render(request,'classes/classes.html',context)
