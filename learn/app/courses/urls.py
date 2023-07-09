6a719c22dad1f774e45c0a49d592335af3b1d96d
Apavayan Sinha <info@apavayan.com>
2023-07-09 08:09:10 +0530

    add app_name in course.urls

--- learn/app/courses/urls.py
+++ learn/app/courses/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
 from . import views
 
+app_name = 'courses'
+
 urlpatterns = [
-    path('classes/',views.classes,name='classes')
+    path('classes/', views.classes, name='classes')
 ]
