184e6826d83c8c92bf51d51e5d40285b1dd80c67
Apavayan Sinha <info@apavayan.com>
2023-07-09 08:08:53 +0530

    Updated Template for Classes

--- learn/app/courses/views.py
+++ learn/app/courses/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views import View
-from .models  import Classes
+from .models import Classes
 # Create your views here.
+
+
 def classes(request):
+    title = 'Classes'
+    breadcrumbs = [{'url': 'core:home',
+                    'label': 'Dashboard'}, {'label': 'Courses'}, {'label': 'Classes'}]
     classes = Classes.objects.all()
-    context = {'classes':classes}
-    return render(request,'classes/classes.html',context)
+    context = {'title': title, 'breadcrumbs': breadcrumbs, 'classes': classes}
+    return render(request, 'classes/index.html', context)
