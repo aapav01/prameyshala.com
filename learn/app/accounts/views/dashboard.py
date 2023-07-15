9c5250231758ac3356e06f37ae928a82d0731da8
Utkarsh Mishra <utkarsh_mishra8@outlook.com>
2023-07-16 01:05:35 +0530

    Added Context for Enrollment & User on dashboard

--- learn/app/accounts/views/dashboard.py
+++ learn/app/accounts/views/dashboard.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.views import View
-
+from ..models import Enrollment, User
+from django.db.models import Sum
 
 @login_required()
 def home(request):
@@ -9,7 +10,12 @@ def home(request):
     breadcrumbs = [{'url': 'core:home',
                     'label': 'Dashboard'}, {'label': 'Home'}]
     context = {'title': title, 'breadcrumbs': breadcrumbs}
+    total_amount = Enrollment.objects.aggregate(Sum('bought_price'))['bought_price__sum']
+    enrollment_count = Enrollment.objects.count()
+    user_count = User.objects.count()
+    context.update({'enrollment_count': enrollment_count, 'user_count':user_count, 'total_amount': total_amount})
     return render(request, 'accounts/home.html', context)
+
 
 @login_required
 def profile(request):
