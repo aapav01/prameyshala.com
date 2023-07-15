fe499dffb2fc6f2c85f731eaefbf002c525cdcfd
Apavayan Sinha <info@apavayan.com>
2023-07-15 22:20:51 +0530

    updated about page

--- app/src/app/about/page.tsx
+++ app/src/app/about/page.tsx
@@ -26,7 +26,7 @@ function AboutPage({}: Props) {
       <section className="container py-12">
         <div className="flex flex-col lg:flex-row h-full items-center">
           <div className="w-full lg:w-1/2 p-10">
-            <div className="mx-auto items-center justify-center flex h-fit">
+            <div className="mx-auto items-center justify-center flex h-fit mb-12">
               <Image
                 src="/img/young-student.png"
                 height={512}
@@ -48,57 +48,84 @@ function AboutPage({}: Props) {
             <span className="text-primary bg-primary/10 px-3 py-1 rounded-2xl text-sm font-semibold">
               About Us
             </span>
-            <h2 className="text-4xl font-sans font-bold py-5">
-              Welcome to the <span className="rock-underline">Online</span>{" "}
-              Learning Center
+            <h2 className="text-4xl md:text-6xl font-sans font-bold py-5">
+              Welcome to <span className="rock-underline">PrameyShala</span>:
             </h2>
-            <div>
-              <p>Meet my startup Edutech agency Webkolek.</p>
+            <div className="text-lg">
+              <strong>Empowering Education Through Technology</strong>
               <p>
-                Welcome to the Online Learning Center, your premier destination
-                for K-12 education in the digital age. With the rapid
-                advancement of technology, online learning has emerged as a
-                transformative force in the field of education. At our center,
-                we are committed to providing a comprehensive and engaging
-                online learning experience for students of all ages. In this
-                webpage, we will explore the features and benefits of our Online
-                Learning Center, showcasing why it is the ideal platform for
-                K-12 education.
+                At Prameyshala, we believe that education is the key to
+                unlocking the full potential of every individual. We are a
+                leading educational technology (EdTech) platform dedicated to
+                revolutionizing the way students learn and teachers teach. Our
+                mission is to make quality education accessible to learners of
+                all ages, backgrounds, and locations.
               </p>
-              <ul className="text-lg py-5">
-                <li className="flex items-baseline">
-                  <CheckCircledIcon className="text-primary mr-1" />
-                  Flexible and Convenient Learning
-                </li>
-                <li className="flex items-baseline">
-                  <CheckCircledIcon className="text-primary mr-1" />
-                  Comprehensive Curriculum
-                </li>
-                <li className="flex items-baseline">
-                  <CheckCircledIcon className="text-primary mr-1" />
-                  Interactive and Engaging Learning Environment
-                </li>
-                <li className="flex items-baseline">
-                  <CheckCircledIcon className="text-primary mr-1" />
-                  Personalized Learning Pathways
-                </li>
-                <li className="flex items-baseline">
-                  <CheckCircledIcon className="text-primary mr-1" />
-                  Safe and Secure Learning Environment
-                </li>
-                <li className="flex items-baseline">
-                  <CheckCircledIcon className="text-primary mr-1" />
-                  Parent and Teacher Engagement
-                </li>
-                <li className="flex items-baseline">
-                  <CheckCircledIcon className="text-primary mr-1" />
-                  Continued Professional Development for Educators
-                </li>
-              </ul>
-              <Button variant={"default"} className="text-lg py-6">
+              <Button variant={"default"} className="text-lg mt-10 py-6">
                 Join For Free
               </Button>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="container py-12">
+        <div className="flex flex-col h-full items-center gap-6">
+          <h2 className="text-4xl md:text-6xl font-sans font-bold py-5">
+            Why Choose <span className="rock-underline">PrameyShala?</span>
+          </h2>
+          <div className="w-full">
+            <ul className="text-xl py-5 list-decimal grid grid-cols-1 md:grid-cols-2 gap-8 px-5">
+              <li>
+                <strong>Comprehensive Learning Solutions:</strong> Prameyshala
+                offers a wide range of comprehensive learning solutions designed
+                to cater to the unique needs of every learner. Whether you are a
+                student preparing for competitive exams, an adult seeking to
+                upskill, or a professional looking for career advancement, we
+                have the right courses and resources for you.
+              </li>
+              <li>
+                <strong>Interactive and Engaging Content:</strong> We understand
+                that effective learning happens when students are engaged and
+                motivated. That&apos;s why we provide interactive and engaging
+                content that combines text, multimedia, and gamification
+                elements to make the learning process enjoyable and effective.
+              </li>
+              <li>
+                <strong>Experienced Educators:</strong> Our team of experienced
+                educators and subject matter experts ensures that the content we
+                provide is accurate, up-to-date, and aligned with the latest
+                educational standards. They bring their expertise and passion
+                for teaching to create high-quality courses that foster
+                conceptual understanding and critical thinking skills.
+              </li>
+              <li>
+                <strong>Personalized Learning Experience:</strong> Prameyshala
+                believes in the power of personalized learning. Our platform
+                uses advanced algorithms to analyze each learner&aplos;s
+                strengths, weaknesses, and learning style to provide
+                personalized recommendations and adaptive learning paths. This
+                approach allows learners to progress at their own pace and focus
+                on areas that need improvement.
+              </li>
+              <li>
+                <strong>Collaborative Learning Community:</strong> Learning is
+                not a solitary process, and Prameyshala understands the
+                importance of collaboration. We provide a vibrant online
+                learning community where students can connect with peers, join
+                discussion forums, and participate in group projects. This
+                fosters a collaborative learning environment that enhances
+                understanding and encourages knowledge sharing.
+              </li>
+              <li>
+                <strong>Continuous Support:</strong> We are committed to
+                providing continuous support to our learners throughout their
+                educational journey. Our dedicated customer support team is
+                available to address any queries or concerns promptly.
+                Additionally, we offer regular assessments, progress tracking,
+                and feedback mechanisms to help learners gauge their performance
+                and make necessary improvements.
+              </li>
+            </ul>
           </div>
         </div>
       </section>
