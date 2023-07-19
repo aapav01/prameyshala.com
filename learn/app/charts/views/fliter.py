25a7f23d6e70ef0706abbe75eb9358842852c08f
Apavayan Sinha <info@apavayan.com>
2023-07-19 19:49:55 +0530

    learn: Fixed Migration bug when importing user model direct to class

--- learn/app/charts/views/fliter.py
+++ learn/app/charts/views/fliter.py
@@ -5,9 +5,12 @@ from .json import JSONView
 
 
 class ChartFliterOptions(JSONView):
-    grouped_users = User.objects.annotate(year=ExtractYear("created_at")).values("year").order_by("-year").distinct()
-    options = [user["year"] for user in grouped_users]
-
-    extra_context = {
-        "options": options
-    }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        grouped_users = User.objects.annotate(year=ExtractYear(
+            "created_at")).values("year").order_by("-year").distinct()
+        options = [user["year"] for user in grouped_users]
+        context.update({
+            "options": options
+        })
+        return context
