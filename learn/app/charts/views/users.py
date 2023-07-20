2e7032a6388342102644fc4ffedc8994766f9d05
Apavayan Sinha <info@apavayan.com>
2023-07-20 20:51:09 +0530

    learn: fixed a bug when year is updated

--- learn/app/charts/views/users.py
+++ learn/app/charts/views/users.py
@@ -7,8 +7,6 @@ from ..util import months, colorPrimary, colorSuccess, colorDanger, generate_col
 
 
 class UserChart(JSONView):
-    users_dict = get_year_dict()
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         year = self.kwargs.get("year")
@@ -16,22 +14,22 @@ class UserChart(JSONView):
         grouped_users = users.annotate(month=ExtractMonth("created_at")).values("month")\
             .annotate(count=Count("id")).order_by("month")
 
-        print(grouped_users)
+        users_dict = get_year_dict()
 
         for group in grouped_users:
-            self.users_dict[months[group["month"]-1]] = group["count"]
+            users_dict[months[group["month"]-1]] = group["count"]
 
         context.update({
             "title": f"Users in {year}",
             "data": {
-                "labels": list(self.users_dict.keys()),
+                "labels": list(users_dict.keys()),
                 "datasets": [{
                     "label": "Users",
                     "backgroundColor": colorPrimary,
                     "borderWidth": 1,
                     "borderRadius": 100,
                     "hoverBackgroundColor": colorSuccess,
-                    "data": list(self.users_dict.values()),
+                    "data": list(users_dict.values()),
                 }]
             },
         })
