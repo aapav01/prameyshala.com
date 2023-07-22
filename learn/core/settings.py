1a08bd674ede8db49e429a48ae3588e758adf702
Apavayan Sinha <info@apavayan.com>
2023-07-22 20:57:38 +0530

    learn: add env varible debug

--- learn/core/settings.py
+++ learn/core/settings.py
@@ -27,9 +27,14 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    '127.0.0.1',
+    'localhost',
+    'prameyshala.com',
+]
 
 
 # Application definition
