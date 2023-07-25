6139e1cfee60b8be07bb8453e7f7aa418a2c5499
Apavayan Sinha <info@apavayan.com>
2023-07-25 22:56:24 +0530

    app: learn view: added loading while session is loaded

--- app/src/app/(Portal)/template.tsx
+++ app/src/app/(Portal)/template.tsx
@@ -11,5 +11,8 @@ export default function Template({ children }: Props) {
   const { status } = useSession({
     required: true,
   });
+  if (status === "loading") return <main className="flex items-center justify-center min-h-[60vh]">
+    <span className="text-2xl font-bold uppercase animate-pulse">Loading...</span>
+  </main>;
   return <>{children}</>;
 }
