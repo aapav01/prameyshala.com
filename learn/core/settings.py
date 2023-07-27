652946bdc6e44286ce78e49251bd4ddaa7499378
Apavayan Sinha <info@apavayan.com>
2023-07-27 20:43:16 +0530

    learn: allow all host

--- learn/core/settings.py
+++ learn/core/settings.py
@@ -29,12 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [
-    '0.0.0.0',
-    '127.0.0.1',
-    'localhost',
-    'prameyshala.com',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
