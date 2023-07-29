976fbb29466f46ffbbc043a7b8899ef40ffd65e2
Apavayan Sinha <info@apavayan.com>
2023-07-29 17:35:19 +0530

    learn: add quiz queryset to get questions_count

--- learn/app/courses/views/quizzes.py
+++ learn/app/courses/views/quizzes.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.db.models import Count
 from ..models import Quiz, Question, Choice
 from ..forms import QuizzesForm
 
@@ -21,10 +22,11 @@ class QuizView(PermissionRequiredMixin, ListView):
     }
     paginate_by = 10
     ordering = ['-created_at']
+    queryset = Quiz.objects.annotate(question_count=Count("question__id"))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        for obj in context['quizzes']:
+        for obj in context[self.context_object_name]:
             temp_form = QuizzesForm(instance=obj)
             obj.form = temp_form
         return context
@@ -38,28 +40,34 @@ class QuizView(PermissionRequiredMixin, ListView):
             instance.save()
             questions_data = []
             for key, value in request.POST.items():
-                if key.startswith('question') and value.strip():  # Check for question field
+                # Check for question field
+                if key.startswith('question') and value.strip():
                     question_number = key[8::1]
                     question_text = value
-                    question_figure = request.FILES.get(f'figure{question_number}')
+                    question_figure = request.FILES.get(
+                        f'figure{question_number}')
 
                     choices_data = []
                     for subkey, subvalue in request.POST.items():
-                        if subkey.startswith(f'choice{question_number}') and subvalue.strip():  # Check for choice field
-                            choice_number = subkey[6::1]
+                        # Check for choice field
+                        if subkey.startswith(f'choice{question_number}') and subvalue.strip():
                             choice_text = subvalue
-                            is_correct_key = f'is_correct-{question_number}'  # Use the question_number from the data attribute
-                            is_correct = request.POST.get(is_correct_key) == choice_text
-                            choices_data.append({'choice_text': choice_text, 'is_correct': is_correct})
-                    questions_data.append({'question_text': question_text, 'question_figure': question_figure, 'choices': choices_data})
+                            # Use the question_number from the data attribute
+                            is_correct_key = f'is_correct-{question_number}'
+                            is_correct = request.POST.get(
+                                is_correct_key) == choice_text
+                            choices_data.append(
+                                {'choice_text': choice_text, 'is_correct': is_correct})
+                    questions_data.append(
+                        {'question_text': question_text, 'question_figure': question_figure, 'choices': choices_data})
 
             # Create and save questions and choices
             for question_data in questions_data:
                 question = Question.objects.create(
-                question_text=question_data['question_text'],
-                figure=question_data['question_figure']
-            )
-
+                    quiz=instance,
+                    question_text=question_data['question_text'],
+                    figure=question_data['question_figure']
+                )
                 for choice_data in question_data['choices']:
                     Choice.objects.create(
                         question=question,
@@ -95,4 +103,3 @@ class QuizDeleteView(PermissionRequiredMixin, DeleteView):
     def get(self, request, **kwargs):
         messages.error(request, 'Quiz has been deleted successfully.')
         return self.delete(request, **kwargs)
-
