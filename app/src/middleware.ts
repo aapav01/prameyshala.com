944fa3ee8f0f3363f9bfe564c96b18795461561a
Apavayan Sinha <info@apavayan.com>
2023-07-31 01:05:55 +0530

    app: using middleware for next-auth

---
+++ app/src/middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export { default } from "next-auth/middleware";
+
+export const config = {
+  matcher: "/learn/:path*",
+};
