3657086a3fbfe3fe10fa3472841d828fb2e61e4a
Apavayan Sinha <info@apavayan.com>
2023-07-31 12:50:12 +0530

    App: add Chapter Page

---
+++ app/src/app/(Portal)/learn/(Enrolled)/chapter/[id]/page.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+
+// GRAPHQL API - APPLOLO
+import { gql } from "@apollo/client";
+import { getClient } from "@/lib/client";
+import LessonCard from "@/components/cards/lessson-card";
+
+const query = gql`
+  query chatpter_detail($id: Int!) {
+    chapter(id: $id) {
+      name
+      subject {
+        name
+        standard {
+          name
+        }
+      }
+      lessonSet {
+        id
+        title
+        lessonType
+      }
+    }
+  }
+`;
+
+type Props = {
+  params: { id: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  // fetch data
+  const { chapter } = await getData({ params });
+
+  return {
+    title:  `${chapter.name} - ${chapter.subject.name} of ${chapter.subject.standard.name} | Pramey Shala`,
+  };
+}
+
+async function getData({ params }: Props) {
+  try {
+    const api_data = await getClient().query({
+      query,
+      variables: { id: parseInt(params.id) },
+    });
+    return api_data.data;
+  } catch (error) {
+    return notFound();
+  }
+}
+
+export default async function ChapterDetail({ params }: Props) {
+  const { chapter } = await getData({ params });
+  return (
+    <>
+      <header className="bg-purple-700 py-6 text-indigo-50 shadow-lg shadow-purple-500/50">
+        <div className="container">
+          <h1 className="text-4xl font-bold">{chapter.name}</h1>
+          <span className="py-2">{chapter.subject.name} of {chapter.subject.standard.name}</span>
+        </div>
+      </header>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 px-4 py-6 gap-4">
+        {chapter.lessonSet.map((lesson: any) => (
+          <LessonCard key={lesson.id} lesson={lesson} />
+        ))}
+      </div>
+    </>
+  );
+}
--- app/src/components/cards/lessson-card.tsx
+++ app/src/components/cards/lessson-card.tsx
@@ -12,22 +12,23 @@ import { FileTextIcon, ActivityLogIcon, PlayIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
 type Props = {
-  chapter: any;
+  lesson: {
+    id: number;
+    title: string;
+    type: string;
+  };
 };
 
-export default function ChapterCard({ chapter }: Props) {
+export default function LessonCard({ lesson }: Props) {
   return (
-    <Link href={"/learn/chapter/" + chapter.id}>
+    <Link href={"/learn/lesson/" + lesson.id}>
       <Card className="animate-in duration-200 ease-in hover:-translate-y-2 hover:shadow-xl hover:shadow-purple-300/50 ">
         <CardHeader className="flex justify-between">
-          <CardTitle>{chapter.name}</CardTitle>
+          <CardTitle>{lesson.title}</CardTitle>
         </CardHeader>
         <CardContent className="mt-2 pb-0">
           <div className="flex flex-row pb-3 text-sm gap-1 justify-between">
-            <span className="inline-flex gap-1">
-              <FileTextIcon className="h-4 w-4 text-purple-600" />
-              <span>{chapter.lessonSet.length} Lessons</span>
-            </span>
+            <span className="inline-flex gap-1">{lesson.type}</span>
             <Button
               className="hover:shadow hover:border-0 hover:shadow-green-500/50 hover:bg-green-700 hover:text-green-200"
               size={"icon"}
