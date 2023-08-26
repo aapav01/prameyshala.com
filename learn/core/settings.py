e8cb3f331d6efc6508f3f9d8f0b26923f6393f45
Apavayan Sinha <info@apavayan.com>
2023-08-26 17:10:57 +0530

    learn: add blog app

--- learn/core/settings.py
+++ learn/core/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'app.accounts.apps.AccountsConfig',
     'app.courses.apps.CoursesConfig',
+    'app.blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
