93a0ea9f0711403601c55fb3bc89417b6bbd3d74
Apavayan Sinha <info@apavayan.com>
2023-09-08 13:35:58 +0530

    app: lesson: assigment: add zoom silder

--- app/src/components/sections/assignment.tsx
+++ app/src/components/sections/assignment.tsx
@@ -5,7 +5,9 @@ import { Document, Page, pdfjs } from "react-pdf";
 
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
+import { Slider } from "@/components/ui/slider"
+import { Label } from "../ui/label";
 
 type Props = {
   file: string;
@@ -19,6 +21,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 export default function Assignment({ file }: Props) {
   const [numPages, setNumPages] = useState<number>(1);
   const [pageNumber, setPageNumber] = useState<number>(1);
+  const [scale, setScale] = useState<number[]>([1.5]);
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
     setNumPages(numPages);
@@ -37,6 +40,36 @@ export default function Assignment({ file }: Props) {
     changePage(1);
   }
 
+  function NavigationBar() {
+    return (
+      <div className="flex justify-between items-center">
+          <p className="p-1">
+            Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+          </p>
+          <div className="w-full max-w-2xl">
+            <Label>Zoom Level {parseInt(`${scale[0] * 100}`)}%</Label>
+            <Slider defaultValue={scale} onValueChange={setScale} min={0.8} max={2} step={0.1} />
+          </div>
+          <div className="flex gap-2">
+            <Button
+              type="button"
+              disabled={pageNumber <= 1}
+              onClick={previousPage}
+            >
+              Previous
+            </Button>
+            <Button
+              type="button"
+              disabled={pageNumber >= numPages}
+              onClick={nextPage}
+            >
+              Next
+            </Button>
+          </div>
+        </div>
+    )
+  }
+
   var source_file = `${process.env.NEXT_PUBLIC_MEDIA_CDN}/static/media/${file}`;
   if (process.env.NODE_ENV === "development") {
     var source_file = `/static/media/${file}`;
@@ -46,57 +79,17 @@ export default function Assignment({ file }: Props) {
     <div className="m-4 p-4 rounded-2xl shadow-xl border-2">
       <div className=" border-b-2 mb-2 pb-4">
         <h2 className="text-2xl font-bold font-sans">Assignment</h2>
-        <div className="flex justify-between items-center">
-          <p className="p-1">
-            Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
-          </p>
-          <div className="flex gap-2">
-            <Button
-              type="button"
-              disabled={pageNumber <= 1}
-              onClick={previousPage}
-            >
-              Previous
-            </Button>
-            <Button
-              type="button"
-              disabled={pageNumber >= numPages}
-              onClick={nextPage}
-            >
-              Next
-            </Button>
-          </div>
-        </div>
+        <NavigationBar />
       </div>
       <Document
         className={"flex flex-col items-center"}
         file={source_file}
         onLoadSuccess={onDocumentLoadSuccess}
       >
-        <Page pageNumber={pageNumber} />
+        <Page scale={scale[0]} pageNumber={pageNumber} />
       </Document>
       <div className=" border-t-2 mt-2 pt-4">
-        <div className="flex justify-between items-center">
-          <p className="p-1">
-            Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
-          </p>
-          <div className="flex gap-2">
-            <Button
-              type="button"
-              disabled={pageNumber <= 1}
-              onClick={previousPage}
-            >
-              Previous
-            </Button>
-            <Button
-              type="button"
-              disabled={pageNumber >= numPages}
-              onClick={nextPage}
-            >
-              Next
-            </Button>
-          </div>
-        </div>
+        <NavigationBar />
       </div>
     </div>
   );
