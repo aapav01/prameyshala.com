cc79bf1fdf49952516d72067da583dd8bfd65035
Apavayan Sinha <info@apavayan.com>
2023-10-01 19:09:53 +0530

    app: supress img tag warning

--- app/src/app/(Landing)/blog/[slug]/page.tsx
+++ app/src/app/(Landing)/blog/[slug]/page.tsx
@@ -64,7 +64,8 @@ export default async function postBySlug({ params }: Props) {
           <section className="w-full md:w-2/3 flex flex-col items-center px-3">
             <article className="flex flex-col shadow my-4">
               <a href="#" className="hover:opacity-75">
-                <img src="https://shorturl.at/TU015" />
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src="https://shorturl.at/TU015" alt={data.postBySlug.slug} />
               </a>
               <div className="bg-white flex flex-col justify-start p-6">
                 <a href="#" className="text-blue-700 text-sm font-bold uppercase pb-4">{data.postBySlug.subtitle}</a>
