d17f343c6ed054c29d26148f79aa8f30075ea3aa
Utkarsh Mishra <utkarsh_mishra8@outlook.com>
2024-02-18 11:05:37 +0530

    learn: added quizdetailview with feature to update quizdetail and add questions
    [#2]

--- learn/app/courses/views/quizzes.py
+++ learn/app/courses/views/quizzes.py
@@ -7,7 +7,7 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.db.models import Count
-from ..forms import ChoiceInlineFormSet, QuestionInlineFormSet, QuestionForm, ChoiceInlineUpdateFormSet
+from ..forms import ChoiceInlineFormSet, QuestionInlineFormSet, QuestionForm, ChoiceInlineUpdateFormSet, QuizForm
 from ..models import Quiz
 from django.db.models import Q
 
@@ -16,16 +16,25 @@ class QuizView(PermissionRequiredMixin, ListView):
     permission_required = "courses.view_quizzes"
     model = Quiz
     template_name = "quizzes/index.html"
+    form = QuizForm
     context_object_name = "quizzes"
     extra_context = {
         'title': 'Quizzes',
         'breadcrumbs': [{'url': 'core:home', 'label': 'Dashboard'}, {'label': 'Courses'}, {'label': 'Quizzes'}],
+        'form': form
     }
     paginate_by = 10
     ordering = ['-created_at']
     queryset = Quiz.objects.annotate(question_count=Count("question__id"))
 
-    #search
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        for obj in context['quizzes']:
+            temp_form = QuizForm(instance=obj, prefix=obj.pk)
+            obj.form = temp_form
+        return context
+    # search
+
     def get_queryset(self):
         queryset = super().get_queryset()
 
@@ -59,11 +68,13 @@ class QuizCreateView(PermissionRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['question_formset'] = QuestionInlineFormSet(prefix='question_formset')
+        context['question_formset'] = QuestionInlineFormSet(
+            prefix='question_formset')
         # context['question_formset'].choice_formset = ChoiceInlineFormSet(prefix='choice_formset_0')
         quesion_count = 0
         for question in context['question_formset']:
-            question.choice_formset = ChoiceInlineFormSet(prefix='choice_formset_%s' % quesion_count)
+            question.choice_formset = ChoiceInlineFormSet(
+                prefix='choice_formset_%s' % quesion_count)
             quesion_count += 1
         return context
 
@@ -86,12 +97,54 @@ class QuizCreateView(PermissionRequiredMixin, CreateView):
         return result
 
 
+class QuizDetailView(PermissionRequiredMixin, DetailView):
+    permission_required = "courses.change_quizzes"
+    model = Quiz
+    template_name = "quizzes/detail.html"
+    context_object_name = "quiz"
+
+    extra_context = {
+        'title': 'Quizzes',
+        'breadcrumbs': [
+            {'url': 'core:home', 'label': 'Dashboard'},
+            {'label': 'Courses'},
+            {'url': 'courses:quizzes', 'label': 'Quizzes'},
+            {'label': 'Detail Quiz View'},
+        ], 'form_question': QuestionForm
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f"{self.object.name}"
+        context['breadcrumbs'][3] = {'label': f"{self.object.name}"}
+        context['form'] = QuizForm(
+            instance=self.object, prefix=self.object.pk)
+        context['questions'] = self.object.question_set.all()
+        return context
+
+    def post(self, request, **kwargs):
+        form = QuestionForm(request.POST)
+        self.extra_context.update({'form_question': form})
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.question = self.get_object()
+            instance.save()
+            messages.success(
+                request, f'{instance.name} has been created successfully.')
+            self.extra_context.update({'form': QuestionForm})
+            return redirect('courses:quiz-detail', pk=self.get_object().pk)
+        else:
+            messages.error(
+                request, f'failed to create! please see the create form for more details.')
+            return super().get(request, **kwargs)
+
 
 class QuizUpdateView(PermissionRequiredMixin, UpdateView):
     permission_required = "courses.change_quizzes"
     model = Quiz
     success_url = reverse_lazy("courses:quizzes")
-    template_name = "quizzes/update_form.html"
+    template_name = "quizzes/form.html"
+    form = QuizForm
     fields = "__all__"
 
     extra_context = {
@@ -104,13 +157,12 @@ class QuizUpdateView(PermissionRequiredMixin, UpdateView):
         ],
     }
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['question_form'] = QuestionForm
-        context['question_form'].choice_formset = ChoiceInlineFormSet
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        prefix = self.kwargs.get('pk')
+        kwargs['prefix'] = prefix
 
-
-        return context
+        return kwargs
 
     def form_valid(self, form):
         result = super().form_valid(form)
