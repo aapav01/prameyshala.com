d32fba0271c3b320d812cf2bb5b0f2db419cee3c
Apavayan Sinha <info@apavayan.com>
2024-03-03 17:31:18 +0530

    app: workaround react-pdf

--- app/next.config.js
+++ app/next.config.js
@@ -1,10 +1,20 @@
+const path = require("path")
+const CopyPlugin = require("copy-webpack-plugin")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.module.rules.push({
-      test: /\.node/,
-      use: 'raw-loader',
-    });
+    config.resolve.alias.canvas = false;
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: require.resolve("pdfjs-dist/build/pdf.worker.min.js"),
+            to: path.join(__dirname, "public/static/js"),
+          },
+        ],
+      }),
+    )
 
     return config;
   },
