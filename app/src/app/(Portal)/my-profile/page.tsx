d1410a9c98bfd0210dcc65bcb8795260bd8b956d
Utkarsh Mishra <utkarsh_mishra8@outlook.com>
2024-03-05 00:05:09 +0530

    app: created user profile page and hamburger menu link to access it.

--- app/src/app/(Portal)/my-profile/page.tsx
+++ app/src/app/(Portal)/my-profile/page.tsx
@@ -15,12 +15,19 @@ const query = gql`
       city
       country
       createdAt
-      isActive
       lastLogin
+      isActive
       phoneNumber
       photo
       state
       updatedAt
+      enrollmentSet {
+        standard {
+          createdAt
+          name
+        }
+        expirationDate
+     }
     }
   }
 `;
@@ -38,38 +45,158 @@ async function getPaymentDetails(session: any) {
     return res as any;
   } catch (error) {
     console.log(error);
-    return error;
+    return { error };
   }
 }
 
-export default async function MyProfilePage({}: Props) {
+export default async function MyProfilePage({ }: Props) {
   const session = await getServerSession(authOptions);
-  const { data } = await getPaymentDetails(session);
+  const { data, error } = await getPaymentDetails(session);
+
+  if (error) {
+    return <div>Error occurred: {error.message}</div>;
+  }
+
+  const lastLogin = data.me?.lastLogin ? new Date(data.me.lastLogin) : null;
+  const lastLoginFormatted = lastLogin ? lastLogin.toISOString().split("T").join(" ").split(".")[0] : "";
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    return date.toISOString().split("T")[0];
+  };
+
   return (
     <div className="container py-4">
-      <div className="border rounded-2xl p-4 max-w-md mx-auto">
-        <div className="flex items-center justify-center">
-        </div>
-        <div className="text-center mt-4">
-          <h1 className="text-xl font-bold">{data.me.fullName}</h1>
-          <p className="text-sm text-gray-500">{data.me.email}</p>
-        </div>
-        <div className="mt-4">
-          <div className="flex justify-between">
-            <p>Phone</p>
-            <p>{data.me.phoneNumber}</p>
+      <div className="relative mt-4 h-96 w-full overflow-hidden rounded-xl bg-cover bg-center">
+        <div className="absolute inset-0 h-full w-full bg-red-500/50"></div>
+      </div>
+      <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-lg mx-3 -mt-16 mb-6 lg:mx-4">
+        <div className="p-4">
+          <div className="mb-6 flex items-center justify-between gap-6">
+            <div className="flex items-center gap-6">
+              <div className="avatar">
+                <div className="w-32 h-32 overflow-hidden rounded-full shadow-lg shadow-blue-gray-500/40">
+                  {data.me?.photo ? (
+                    <img
+                      src={data.me.photo}
+                      width="78px"
+                      height="78px"
+                      alt={data.me.fullName}
+                    />
+                  ) : (
+                    <img
+                      src="https://placehold.co/600x600"
+                      width="144px"
+                      height="144px"
+                      alt={data.me.fullName}
+                    />
+                  )}
+                </div>
+              </div>
+              <div>
+                <h5 className="block antialiased tracking-normal font-serif text-2xl font-semibold leading-snug text-blue-gray-900">
+                  {data.me?.fullName}
+                </h5>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-between">
-            <p>City</p>
-            <p>{data.me.city}</p>
+          <div className="grid grid-cols-1 mb-10 gap-10 px-4 lg:grid-cols-1">
+            <div className="relative flex flex-col bg-clip-border bg-transparent text-gray-700 shadow-md p-8 rounded-xl border border-slate-200">
+              <div className="relative bg-clip-border overflow-hidden bg-transparent text-gray-700 shadow-none mx-0 mt-0 flex items-center justify-between gap-4">
+                <h6 className="block antialiased tracking-normal font-serif text-xl font-semibold leading-relaxed text-blue-gray-900">
+                  Profile Information
+                </h6>
+                <a className="btn btn-ghost btn-sm btn-square">
+                  <i className="fad fa-pencil"></i>
+                </a>
+              </div>
+              <div className="p-0">
+                <hr className="my-4 border-blue-gray-50" />
+                <ul className="flex flex-col gap-4 p-0">
+                  <li className="flex items-center gap-4">
+                    <p className="block antialiased font-serif text-lg leading-normal text-blue-gray-900 font-semibold capitalize">
+                      First Name:
+                    </p>
+                    <p className="block antialiased font-serif text-lg leading-normal font-normal text-gray-400">
+                      {data.me?.fullName}
+                    </p>
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <p className="block antialiased font-serif text-lg leading-normal text-blue-gray-900 font-semibold capitalize">
+                      Mobile:
+                    </p>
+                    <p className="block antialiased font-serif text-lg leading-normal font-normal text-gray-400">
+                      {data.me?.phoneNumber}
+                    </p>
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <p className="block antialiased font-serif text-lg leading-normal text-blue-gray-900 font-semibold capitalize">
+                      Email:
+                    </p>
+                    <p className="block antialiased font-serif text-lg leading-normal font-normal text-gray-400">
+                      {data.me?.email}
+                    </p>
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <p className="block antialiased font-serif text-lg leading-normal text-blue-gray-900 font-semibold capitalize">
+                      Last Login:
+                    </p>
+                    <p className="block antialiased font-serif text-lg leading-normal font-normal text-gray-400">
+                      {lastLoginFormatted}
+                    </p>
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <p className="block antialiased font-serif text-lg leading-normal text-blue-gray-900 font-semibold capitalize">
+                      Joined On:
+                    </p>
+                    <p className="block antialiased font-serif text-lg leading-normal font-normal text-gray-400">
+                      {formatDate(data.me?.createdAt)}
+                    </p>
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <p className="block antialiased font-serif text-lg leading-normal text-blue-gray-900 font-semibold capitalize">
+                      is Active:
+                    </p>
+                    <p className="block antialiased font-serif text-lg leading-normal font-normal text-gray-400">
+                      {data.me?.isActive ? "Yes" : "No"}
+                    </p>
+                  </li>
+                  <li className="flex items-center gap-4">
+                    <p className="block antialiased font-serif text-lg leading-normal text-blue-gray-900 font-semibold capitalize">
+                      Country:
+                    </p>
+                    <p className="block antialiased font-serif text-lg leading-normal font-normal text-gray-400">
+                      {data.me?.country}
+                    </p>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-between">
-            <p>State</p>
-            <p>{data.me.state}</p>
-          </div>
-          <div className="flex justify-between">
-            <p>Country</p>
-            <p>{data.me.country}</p>
+          <div className="px-4 pb-4">
+            <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-blue-gray-900 mb-2">
+              Your Enrolled Courses
+            </h6>
+            <p className="block antialiased mb-1 font-sans text-sm leading-normal font-normal text-blue-gray-500">
+              You have enrolled in {data.me?.enrollmentSet.length || 0} courses
+            </p>
+            <div className="mt-6 grid grid-cols-2 gap-4">
+              {data.me?.enrollmentSet.map((enrolled: any) => (
+                <div key={enrolled.id} className="flex flex-col card shadow p-4 gap-1 rounded-xl border border-slate-200">
+                  <span className="text-xl font-bold">{enrolled.standard?.name}</span>
+                  <ul className="flex flex-col gap-1 p-0">
+                    <li className="flex items-center gap-1">
+                      <p className="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-semibold capitalize">Created At:</p>
+                      <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-500">{formatDate(enrolled.standard?.createdAt)}</p>
+                    </li>
+                    <li className="flex items-center gap-1">
+                      <p className="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-semibold capitalize">Expiration Date:</p>
+                      <p className="block antialiased font-sans text-sm leading-normal font-normal text-blue-gray-500">{enrolled.expirationDate}</p>
+                    </li>
+                  </ul>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
